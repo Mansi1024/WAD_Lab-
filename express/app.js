713cc3b01ca1a9dbc6343e25e08d596d5c69c504
Mansi Nikam <nikammansi412@gmail.com>
2024-02-27 14:28:35 +0530

    added

---
+++ express/app.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const port =3000;
+const app=express();
+// app.use(express.static('public'))
+app.get("/",(req,res) => {
+    res.sendFile(__dirname+"/public/index.html")
+})
+
+app.get("/about",(req,res)=>{
+    res.sendFile(__dirname+"/public/about.html")
+})
+
+
+app.get("/contact",(req,res) => {
+    res.sendFile(__dirname+"/public/contact.html")
+})
+app.listen(port);
